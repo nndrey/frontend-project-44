35bb9d7bc1b1b89531c3f8a8cccdaa3e651f662e
nndrey <ordosmaleus@yandex.ru>
2024-11-23 14:37:04 +0300

    correction of an error in brainPrime

--- src/games/brainPrime.js
+++ src/games/brainPrime.js
@@ -2,7 +2,7 @@ import { brainGamesEngine } from "../index.js";
 import { random, isPrimeNumber } from "../helpers.js";
 
 const doPrime = () => {
-  const rules = '"yes" if given number is prime. Otherwise answer "no".';
+  const rules = 'Answer "yes" if given number is prime. Otherwise answer "no"';
   const randomNumber = random(1, 100);
   const correctAnswer = isPrimeNumber(randomNumber) ? "yes" : "no";
   const question = randomNumber;
